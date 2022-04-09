e3b783e23fe8d6e5439260ca35017ca2af687aee
Bruno Cabral <cabral.brc@gmail.com>
2022-04-09 14:26:51 -0700

    line struct with 3d projection

--- main.cpp
+++ main.cpp
@@ -3,9 +3,33 @@
 using namespace std;
 using namespace sf;
 
-int width = 1024;
-int height = 768;
+int screen_width = 1024;
+int screen_height = 768;
 
+int roadW = 2000;  // road width (left to right)
+int segL = 200;    // segment length (top to bottom)
+float camD = 0.84; // camera depth
+
+struct Line
+{
+    float x, y, z; // game position (3D space)
+    float X, Y, W; // screen position (2D space)
+    float scale;
+
+    Line() { x = y = z = 0; }
+
+    // from game pos to screen pos
+    void project(int camX, int camY, int camZ)
+    {
+        scale = camD / (z - camZ);
+        X = (1 + scale * (x - camX)) * screen_width / 2;
+        Y = (1 - scale * (y - camY)) * screen_height / 2;
+        W = scale * roadW * screen_width / 2;
+    }
+};
+
+// Draws a quadratic polygon on screen
+//
 void drawQuad(RenderWindow &w, Color c, int x1, int y1, int w1, int x2, int y2, int w2)
 {
     ConvexShape shape(4.f);
@@ -19,7 +43,7 @@ void drawQuad(RenderWindow &w, Color c, int x1, int y1, int w1, int x2, int y2, 
 
 int main()
 {
-    RenderWindow app(VideoMode(width, height), "Outrun Racing!");
+    RenderWindow app(VideoMode(screen_width, screen_height), "Outrun Racing!");
     app.setFramerateLimit(60);
 
     while (app.isOpen())
